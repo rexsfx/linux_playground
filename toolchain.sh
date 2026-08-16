699b1830b1d3697f2aed0d9bf7d4d2d416089f40
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-16 17:41:30 +0700

    Add 'patch' package to installation command

--- toolchain.sh
+++ toolchain.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #setup mandatory packages
-sudo pacman -Sy --noconfirm git llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait
+sudo pacman -Sy --noconfirm git patch llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait
 #Toolchain compiler
 wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait
 mkdir -p clang
@@ -13,7 +13,6 @@ tar --zstd -xf sakura-clang-24.0.0.tar.zst
 wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/mlgo-models.tar.gz
 tar -zxvf mlgo-models.tar.gz
 cd ..
-
 
 #test clang
 export PATH=$(pwd)/clang/bin:$PATH
