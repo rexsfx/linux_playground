484fae93d5f091ddcaabe1320b4f4bef10d416b4
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-16 17:17:10 +0700

    Update toolchain.sh to use --noconfirm with pacman

--- toolchain.sh
+++ toolchain.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #setup mandatory packages
-sudo pacman -Sy git llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait;wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait; mkdir -p clang; mv sakura-clang-24.0.0.tar.zst; tar --zstd -xf sakura-clang-24.0.0.tar.zst 
+sudo pacman -Sy --noconfirm git llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait;wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait; mkdir -p clang; mv sakura-clang-24.0.0.tar.zst; tar --zstd -xf sakura-clang-24.0.0.tar.zst 
