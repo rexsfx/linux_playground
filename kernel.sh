4eba09741a5427171e418a1cef572bcbc12417eb
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-16 18:51:51 +0700

    Add NOMOUNT support and installation in kernel.sh
    Added NOMOUNT configuration and installation script.

--- kernel.sh
+++ kernel.sh
@@ -23,6 +23,7 @@ echo "CONFIG_KSU_MANUAL_HOOK=y" >> $defconfig_path
 wget https://raw.githubusercontent.com/xxblebleblexx/manual_hook_fix/refs/heads/main/resuki-4.19-cip-st.patch;wait;patch -p1 < resuki-4.19-cip-st.patch
 
 #Nomount driver
+echo "CONFIG_NOMOUNT=y" >> $defconfig_path
 curl -LSs "https://raw.githubusercontent.com/xxblebleblexx/nomount-installer/refs/heads/dev-installer/nomount.sh" | bash -s 4.19
 
 #Run compile
