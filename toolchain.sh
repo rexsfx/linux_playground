34a5f198a949d4f050d55767b6cd8fb49eade5dd
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-16 17:13:39 +0700

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -2,6 +2,4 @@
 
 #setup mandatory packages
 sudo pacman -Sy git llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget
-
-#setup Clang (Toolchain/compiler)
 wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst; mkdir -p clang; mv sakura-clang-24.0.0.tar.zst; tar --zstd -xf sakura-clang-24.0.0.tar.zst 
