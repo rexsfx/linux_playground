2a8e699d9bd404ec4be35076beb2084f3e44f610
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-16 17:15:08 +0700

    Update toolchain.sh to include wait commands

--- toolchain.sh
+++ toolchain.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 #setup mandatory packages
-sudo pacman -Sy git llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget
-wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst; mkdir -p clang; mv sakura-clang-24.0.0.tar.zst; tar --zstd -xf sakura-clang-24.0.0.tar.zst 
+sudo pacman -Sy git llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait;wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait; mkdir -p clang; mv sakura-clang-24.0.0.tar.zst; tar --zstd -xf sakura-clang-24.0.0.tar.zst 
