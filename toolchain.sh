d6ee54a86365ba68cc8e11473f15fbc4fb6035e4
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-08-16 17:22:27 +0700

    Refactor toolchain.sh for better readability

--- toolchain.sh
+++ toolchain.sh
@@ -3,4 +3,9 @@
 #setup mandatory packages
 sudo pacman -Sy --noconfirm git llvm lld base-devel ncurses openssl bc flex bison rsync kmod cpio python ccache tar gzip zstd wget;wait
 #Toolchain compiler
-wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait; mkdir -p clang; mv sakura-clang-24.0.0.tar.zst clang;cd clang; tar --zstd -xf sakura-clang-24.0.0.tar.zst; cd 
+wget https://github.com/Madara273/Sakura-Clang-Compiler/releases/download/Sakura-Clang-R01/sakura-clang-24.0.0.tar.zst;wait
+mkdir -p clang
+mv sakura-clang-24.0.0.tar.zst clang/
+cd clang
+tar --zstd -xf sakura-clang-24.0.0.tar.zst
+cd ..
